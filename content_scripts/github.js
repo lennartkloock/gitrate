d6cca1c1f520a141abba16481cc6e7d43b49c3e4
Lennart Kloock <lennart.kloock@protonmail.com>
2022-10-16 14:45:43 +0200

    More Cleanup and refactor

--- content_scripts/github.js
+++ content_scripts/github.js
@@ -11,18 +11,17 @@ function inject() {
     if (isRepo() && !document.getElementById('reviews-tab')) {
         let paramString = location.href.split('?')[1];
         let queryString = new URLSearchParams(paramString);
-        let enabled = queryString.get('tab') === 'reviews';
-        if (enabled) {
-            deselectAllTabs();
-        }
-        injectReviewsTab(enabled, 0);
+        injectReviewsTab(queryString.get('tab') === 'reviews', 0);
     }
 }
 
 function injectReviewsTab(enabled, count) {
     const header = document.getElementById('repository-container-header');
-    let linkList = document.evaluate('nav/ul', header).iterateNext();
-    if (linkList) {
+    let tabList = document.evaluate('nav/ul', header).iterateNext();
+    if (tabList) {
+        if (enabled) {
+            deselectAllTabs(tabList);
+        }
         loadHtml('/reviews-tab.html',
             {
                 enabled: enabled ? 'page' : 'false',
@@ -33,10 +32,10 @@ function injectReviewsTab(enabled, count) {
             },
         )
             .then((html) => {
-                linkList.appendChild(html);
+                tabList.appendChild(html);
                 let element = document.getElementById('reviews-tab');
                 element.onclick = () => {
-                    deselectAllTabs();
+                    deselectAllTabs(tabList);
                     history.pushState({}, '', '?tab=reviews');
                     element.classList.add('selected');
                     element.setAttribute('aria-current', 'page');
@@ -45,11 +44,8 @@ function injectReviewsTab(enabled, count) {
     }
 }
 
-function deselectAllTabs() {
-    const header = document.getElementById('repository-container-header');
-    let linkList = document.evaluate('nav/ul', header).iterateNext();
-
-    const xpath = document.evaluate('li/a', linkList, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+function deselectAllTabs(tabList) {
+    const xpath = document.evaluate('li/a', tabList, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
     for (let i = 0; i < xpath.snapshotLength; i++) {
         let link = xpath.snapshotItem(i);
         link.classList.remove('selected');
